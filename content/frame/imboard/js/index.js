7cb9acb5c8e22383fecd61132ac16cf1bed00572
Alprensia <tonite32@gmail.com>
2015-07-08 15:16:30 +0900

    임보드 배포준비

--- content/frame/imboard/js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function()
-{
-	if($.query.fragment)
-	{
-		$(".navbar-nav li").removeAttr("active");
-		$(".navbar-nav li a[href='?fragment=" + $.query.fragment + "']").parent().addClass("active");
-	}
-});
