7d1f8939daeaa60851f66b335dd7f6ee5c784949
Alprensia <tonite32@gmail.com>
2015-07-18 17:20:45 +0900

    관리자페이지 게시판관리화면 오류 수

--- content/common/settings/js/board.js
+++ content/common/settings/js/board.js
@@ -4,8 +4,12 @@ $(document).ready(function()
 
 	$("#addBoardButton").on("click", function()
 	{
-		var clone = $("#boardList").children("div:first").clone(true);
-		$("#boardList").prepend(clone);
+		var roleList = $.api.role.getRoleList();
+		var html = $("#settings-board-template").html();
+		var template = Handlebars.compile(html);
+		$("#boardList").prepend(template({boardList : [{}], roleList : roleList.data}));
+		
+		var clone = $("#boardList form:first");
 		
 		clone.find(".panel").attr("class", "panel panel-primary");
 		
@@ -15,15 +19,16 @@ $(document).ready(function()
 		clone.find(".panel-footer input").unbind();
 		clone.find(".panel-footer input[value='저장']").attr("data-role", "submit");
 		clone.find(".panel-footer input[value='삭제']").val("취소");
+		clone.find("input[type='radio'][value='Y']").get(0).checked = true;
 		
-		var form = clone.find("form").get(0);
+		var form = clone.get(0);
 		
 		$(form).removeAttr("novalidate").attr("data-component", "form");
 		$(form).compile(function(data)
 		{
 			if(!data.id)
 				data.id = new Date().getTime();
-			
+
 			var result = $.api.board.insertBoard(data);
 			
 			if(result.code == 1000)
@@ -37,34 +42,6 @@ $(document).ready(function()
 				$("#result").text(JSON.stringify(result)).css("color", "white");
 			}
 		});
-		
-//		var template = Handlebars.compile(html);
-//		var roleList = $.api.role.getRoleList();
-//		$("#board > div").prepend(template({roleList : roleList}));
-//		
-//		$("#newForm").compile(function(data)
-//		{
-//			if(!data.id)
-//				data.id = new Date().getTime();
-//			
-//			var result = $.api.board.insertBoard(data);
-//			
-//			if(result.code == 1000)
-//			{
-//				data.boardId = data.id;
-//				$.api.boardAuth.updateBoardAuth(data);
-//				refresh();
-//			}
-//			else
-//			{
-//				$("#result").text(JSON.stringify(result));
-//			}
-//		});
-//		
-//		$("#cancel").on("click", function()
-//		{
-//			$("#board > div > div:first").remove();
-//		});
 	});
 });
 
@@ -122,42 +99,4 @@ function refresh()
 		$("#boardList").templating("settings-board-template", {boardList : result.data});
 		initForm();
 	}
-}
-
-function init()
-{
-//	$("#board button[data-id='save']").on("click", function()
-//	{
-//		var parent = $(this).parent().parent();
-//		var boardId = parent.find("span[data-id='boardId']").text();
-//		var boardName = parent.find("input[data-id='boardName']").val();
-//		var priority = parent.find("input[data-id='priority']").val();
-//		var viewListLevel = parent.find("select[data-id='viewListLevel']").val();
-//		var viewDetailLevel = parent.find("select[data-id='viewDetailLevel']").val();
-//		var writeLevel = parent.find("select[data-id='writeLevel']").val();
-//		var writeCommentLevel = parent.find("select[data-id='writeCommentLevel']").val();
-//		var useYn = parent.find("input[type='radio'][name^='useYn_']:checked").val();
-//		
-//		var result = $.api.board.updateBoard({id : boardId, name : boardName, priority : priority, useYn : useYn});
-//		
-//		if(result.code == "1000")
-//		{
-//			var param = {boardId : boardId, viewListLevel : viewListLevel, viewDetailLevel : viewDetailLevel, writeLevel : writeLevel, writeCommentLevel : writeCommentLevel};
-//			$.api.boardAuth.updateBoardAuth(param);
-//			$(this).parent().children("span").css("color", "gray").text("Updated");
-//		}
-//		else
-//		{
-//			$(this).parent().children("span").css("color", "red").text("Update fail");
-//		}
-//	});
-//	
-//	$("#board button[data-id='delete']").on("click", function()
-//	{
-//		var parent = $(this).parent().parent();
-//		var boardId = parent.find("span[data-id='boardId']").text();
-//		
-//		$.api.board.deleteBoard({imboardId : _globalData.imboard.id, boardId : boardId});
-//		init();
-//	});
 }
