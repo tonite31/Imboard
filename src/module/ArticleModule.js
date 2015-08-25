9a470b4d5e25c6ed27f9a2c031c9d2ac4acd38f0
Alprensia <tonite32@gmail.com>
2015-08-25 14:13:35 +0900

    ArticleModule 권한체크로직 수정

--- src/module/ArticleModule.js
+++ src/module/ArticleModule.js
@@ -12,12 +12,40 @@ module.exports.articleList = function($, el, param, req, next)
 	var boardId = param.boardId;
 	
 	var searchData = param.searchData;
-	ArticleDao.getArticleList(boardId, searchData, function(list)
+	var checkAuth = function(cb)
 	{
-		var template = that.getTemplate($, el);
-		$(el).html(template({articleList : list}));
-		next();
-	});
+		BoardAuthDao.getBoardAuth(boardId, function(response)
+		{
+			if(response && response.viewListlLevel != null)
+			{
+				if(req.session.user && req.session.user.level <= response.viewListlLevel)
+				{
+					cb();
+				}
+				else
+				{
+					$(el).html("<p class='databind-error'>Access denied.</p>");
+					next();
+				}
+			}
+			else
+			{
+				cb();
+			}
+		});
+	};
+	
+	var getArticleList = function(cb)
+	{
+		ArticleDao.getArticleList(boardId, searchData, function(list)
+		{
+			var template = that.getTemplate($, el);
+			$(el).html(template({articleList : list}));
+			next();
+		});
+	}
+	
+	_async.waterfall([checkAuth, getArticleList]);
 };
 
 module.exports.articleComponent = function($, el, param, req, next)
@@ -26,126 +54,74 @@ module.exports.articleComponent = function($, el, param, req, next)
 	var boardId = param.boardId;
 	var registerDateType = param.registerDateType;
 	
-	var getArticleList = function()
+	var checkAuth = function(cb)
 	{
-		var pageIndex = req.query.pageIndex;
-		if(pageIndex == null || isNaN(pageIndex))
-			pageIndex = 1;
-		
-		var searchData = param.searchData;
-		var cpp = param.cpp;
-		
-		if(cpp)
-			cpp = parseInt(cpp);
-		else
-			cpp = 10;
-		
-		if(searchData == null)
-			searchData = {};
-
-		if(searchData.tag != null)
+		BoardAuthDao.getBoardAuth(boardId, function(response)
 		{
-			var tag = "";
-			for(var i=0; i<searchData.tag.length; i++)
+			if(response && response.viewListlLevel != null)
 			{
-				if(i > 0)
-					tag += " AND ";
-				tag += "TAG REGEXP '" + searchData.tag[i] + "'";
-			}
-			
-			searchData.tag = tag;
-		}
-		
-		var data = {
-			boardId : boardId,
-			searchData : searchData
-		};
-		
-		data.searchData.registerDateType = registerDateType;
-		
-		var setPagination = function(cb)
-		{
-			if(param.pagination != "false")
-			{
-				var pgc = param.pgc;
-				
-				if(pgc != null && pgc != "")
-					pgc = parseInt(pgc);
-				else
-					pgc = 10;
-				
-				if(req.session.user != null)
+				if(req.session.user && req.session.user.level <= response.viewListlLevel)
 				{
-					data.searchData.signinUserId = req.session.user.id;
-					data.searchData.signinUserLevel = req.session.user.level;
-				}
-				
-				ArticleDao.getArticleListCount(data.boardId, data.searchData, function(totalCount)
-				{
-					var maxPage = Math.ceil(totalCount / cpp);
-					maxPage = maxPage == 0 ? 1 : maxPage;
-						
-					
-					var startPage = Math.ceil(pageIndex / pgc);
-					startPage = startPage + (startPage - 1) * (pgc - 1);
-					
-					var pageList = [];
-					for(var i=startPage; i<startPage + pgc && i <= maxPage; i++)
-					{
-						pageList.push({pageNumber : i, href : "?piece=" + req.query.piece + "&boardId=" + data.boardId + "&pageIndex=" + i});
-					}
-					
-					$(el).find("*[data-parts='pagination']").each(function()
-					{
-						$(this).removeAttr("data-parts");
-						
-						var template = that.getTemplate($, this);
-						$(this).html(template({pageList : pageList, pageIndex : parseInt(pageIndex), maxPage : maxPage}));
-					});
-					
 					cb();
-				});
-				
-//				var totalCount = $.api.article.getArticleListCount(data);
+				}
+				else
+				{
+					$(el).html("<p class='databind-error'>Access denied.</p>");
+					next();
+				}
 			}
 			else
 			{
 				cb();
 			}
+		});
+	};
+	
+	var pageIndex = req.query.pageIndex;
+	if(pageIndex == null || isNaN(pageIndex))
+		pageIndex = 1;
+	
+	var searchData = param.searchData;
+	var cpp = param.cpp;
+	
+	if(cpp)
+		cpp = parseInt(cpp);
+	else
+		cpp = 10;
+	
+	if(searchData == null)
+		searchData = {};
+
+	if(searchData.tag != null)
+	{
+		var tag = "";
+		for(var i=0; i<searchData.tag.length; i++)
+		{
+			if(i > 0)
+				tag += " AND ";
+			tag += "TAG REGEXP '" + searchData.tag[i] + "'";
 		}
 		
-		var setArticleList = function(cb)
+		searchData.tag = tag;
+	}
+	
+	var data = {
+		boardId : boardId,
+		searchData : searchData
+	};
+	
+	data.searchData.registerDateType = registerDateType;
+	
+	var setPagination = function(cb)
+	{
+		if(param.pagination != "false")
 		{
-			data.searchData.cpp = cpp;
-			data.searchData.pageIndex = pageIndex;
+			var pgc = param.pgc;
 			
-			if(param.searchData != null)
-			{
-				var searchData = param.searchData;
-				for(var key in searchData)
-				{
-					data.searchData[key] = searchData[key];
-				}
-			}
-			
-			if(param.withContent == "true")
-				data.searchData.withContent = "true";
-			
-//			var list = $.api.article.__getArticleList(data);
-			
-			try
-			{
-				if(pageIndex != null && cpp != null)
-				{
-					data.searchData.startIndex = parseInt(cpp) * (parseInt(pageIndex) -1);
-					data.searchData.endIndex = parseInt(cpp);
-				}
-			}
-			catch(err)
-			{
-				data.searchData.startIndex = null;
-				data.searchData.endIndex = null;
-			}
+			if(pgc != null && pgc != "")
+				pgc = parseInt(pgc);
+			else
+				pgc = 10;
 			
 			if(req.session.user != null)
 			{
@@ -153,46 +129,116 @@ module.exports.articleComponent = function($, el, param, req, next)
 				data.searchData.signinUserLevel = req.session.user.level;
 			}
 			
-			ArticleDao.getArticleList(data.boardId, data.searchData, function(list)
+			ArticleDao.getArticleListCount(data.boardId, data.searchData, function(totalCount)
 			{
-				var divideCount = parseInt(param.divideCount);
-				if(!isNaN(divideCount))
+				var maxPage = Math.ceil(totalCount / cpp);
+				maxPage = maxPage == 0 ? 1 : maxPage;
+					
+				
+				var startPage = Math.ceil(pageIndex / pgc);
+				startPage = startPage + (startPage - 1) * (pgc - 1);
+				
+				var pageList = [];
+				for(var i=startPage; i<startPage + pgc && i <= maxPage; i++)
 				{
-					var bodyList = $(el).find("*[data-parts='body']");
-					var index = 0;
-					for(var i=0; i<list.length; i++)
-					{
-						var template = bodyList[index].template;
-						if(!template)
-						{
-							template = that.getTemplate($, bodyList[index]);
-							bodyList[index].template = template;
-						}
-						
-						$(bodyList[index++]).append(template(list[i]));
-						if(index == divideCount)
-							index = 0;
-					}
-				}
-				else
-				{
-					$(el).find("*[data-parts='body']").each(function()
-					{
-						$(this).removeAttr("data-parts");
-						
-						var template = that.getTemplate($, this);
-						$(this).html(template({articleList : list}));
-					});
+					pageList.push({pageNumber : i, href : "?piece=" + req.query.piece + "&boardId=" + data.boardId + "&pageIndex=" + i});
 				}
 				
-				next();
+				$(el).find("*[data-parts='pagination']").each(function()
+				{
+					$(this).removeAttr("data-parts");
+					
+					var template = that.getTemplate($, this);
+					$(this).html(template({pageList : pageList, pageIndex : parseInt(pageIndex), maxPage : maxPage}));
+				});
+				
+				cb();
 			});
-		};
+			
+//				var totalCount = $.api.article.getArticleListCount(data);
+		}
+		else
+		{
+			cb();
+		}
+	}
+	
+	var setArticleList = function(cb)
+	{
+		data.searchData.cpp = cpp;
+		data.searchData.pageIndex = pageIndex;
 		
-		_async.waterfall([setPagination, setArticleList]);
+		if(param.searchData != null)
+		{
+			var searchData = param.searchData;
+			for(var key in searchData)
+			{
+				data.searchData[key] = searchData[key];
+			}
+		}
+		
+		if(param.withContent == "true")
+			data.searchData.withContent = "true";
+		
+//			var list = $.api.article.__getArticleList(data);
+		
+		try
+		{
+			if(pageIndex != null && cpp != null)
+			{
+				data.searchData.startIndex = parseInt(cpp) * (parseInt(pageIndex) -1);
+				data.searchData.endIndex = parseInt(cpp);
+			}
+		}
+		catch(err)
+		{
+			data.searchData.startIndex = null;
+			data.searchData.endIndex = null;
+		}
+		
+		if(req.session.user != null)
+		{
+			data.searchData.signinUserId = req.session.user.id;
+			data.searchData.signinUserLevel = req.session.user.level;
+		}
+		
+		ArticleDao.getArticleList(data.boardId, data.searchData, function(list)
+		{
+			var divideCount = parseInt(param.divideCount);
+			if(!isNaN(divideCount))
+			{
+				var bodyList = $(el).find("*[data-parts='body']");
+				var index = 0;
+				for(var i=0; i<list.length; i++)
+				{
+					var template = bodyList[index].template;
+					if(!template)
+					{
+						template = that.getTemplate($, bodyList[index]);
+						bodyList[index].template = template;
+					}
+					
+					$(bodyList[index++]).append(template(list[i]));
+					if(index == divideCount)
+						index = 0;
+				}
+			}
+			else
+			{
+				$(el).find("*[data-parts='body']").each(function()
+				{
+					$(this).removeAttr("data-parts");
+					
+					var template = that.getTemplate($, this);
+					$(this).html(template({articleList : list}));
+				});
+			}
+			
+			next();
+		});
 	};
 	
-	_async.waterfall([getArticleList]);
+	_async.waterfall([checkAuth, setPagination, setArticleList]);
 };
 
 module.exports.article = function($, el, param, req, next)
@@ -201,83 +247,102 @@ module.exports.article = function($, el, param, req, next)
 	
 	var boardId = param.boardId;
 	var seq = param.seq;
-	
-	var bindArticle = function()
+
+	var checkAuth = function(cb)
 	{
-		var getArticle = function(cb)
+		BoardAuthDao.getBoardAuth(boardId, function(response)
 		{
-			var searchData = {};
-			if(req.session.user != null)
+			if(response && response.viewDetailLevel != null)
 			{
-				searchData.signinUserId = req.session.user.id;
-				searchData.signinUserLevel = req.session.user.level;
-			}
-			
-			ArticleDao.getArticle(boardId, seq, searchData, function(article)
-			{
-				if(article)
+				if(req.session.user && req.session.user.level <= response.viewDetailLevel)
 				{
-					if(article.password != null)
-					{
-						if((req.session.user != null && req.session.user.level < 0) || article.password == param.password)
-						{
-							var template = that.getTemplate($, el);
-							$(el).html(template(article));
-							cb();
-						}
-						else
-						{
-							next();
-						}
-					}
-					else
-					{
-						var template = that.getTemplate($, el);
-						$(el).html(template(article));
-						cb();
-					}
+					cb();
 				}
 				else
 				{
 					$(el).html("<p class='databind-error'>Access denied.</p>");
 					next();
 				}
-			});
-		};
-		
-		var updateHit = function(cb)
-		{
-			var userId = null;
-			if(req.session.user != null && req.session.user.id != null)
-				userId = req.session.user.id;
+			}
 			else
-				userId = (req.headers['x-forwarded-for'] || req.connection.remoteAddress) + "__" + req.headers['user-agent'];
-			
-			ArticleReaderDao.getArticleReader(boardId, seq, userId, function(articleReader)
 			{
-				if(articleReader == null)
+				cb();
+			}
+		});
+	};
+	
+	var getArticle = function(cb)
+	{
+		var searchData = {};
+		if(req.session.user != null)
+		{
+			searchData.signinUserId = req.session.user.id;
+			searchData.signinUserLevel = req.session.user.level;
+		}
+		
+		ArticleDao.getArticle(boardId, seq, searchData, function(article)
+		{
+			if(article)
+			{
+				if(article.password != null)
 				{
-					var vo = new ArticleReaderVo();
-					vo.boardId = boardId;
-					vo.articleSeq = seq;
-					vo.userId = userId;
-					
-					ArticleReaderDao.insertArticleReader(vo);
-					
-					ArticleDao.updateHit(vo.boardId, vo.articleSeq, function()
+					if((req.session.user != null && req.session.user.level < 0) || article.password == param.password)
+					{
+						var template = that.getTemplate($, el);
+						$(el).html(template(article));
+						cb();
+					}
+					else
 					{
 						next();
-					});
+					}
 				}
 				else
 				{
-					next();
+					var template = that.getTemplate($, el);
+					$(el).html(template(article));
+					cb();
 				}
-			});
-		};
-		
-		_async.waterfall([getArticle, updateHit]);
+			}
+			else
+			{
+				var template = that.getTemplate($, el);
+				$(el).html(template(article));
+				next();
+			}
+		});
 	};
 	
-	_async.waterfall([bindArticle]);
+	var updateHit = function(cb)
+	{
+		var userId = null;
+		if(req.session.user != null && req.session.user.id != null)
+			userId = req.session.user.id;
+		else
+			userId = (req.headers['x-forwarded-for'] || req.connection.remoteAddress) + "__" + req.headers['user-agent'];
+		
+		ArticleReaderDao.getArticleReader(boardId, seq, userId, function(articleReader)
+		{
+			if(articleReader == null)
+			{
+				var vo = new ArticleReaderVo();
+				vo.boardId = boardId;
+				vo.articleSeq = seq;
+				vo.userId = userId;
+				
+				ArticleReaderDao.insertArticleReader(vo);
+				
+				ArticleDao.updateHit(vo.boardId, vo.articleSeq, function()
+				{
+					next();
+				});
+			}
+			else
+			{
+				next();
+			}
+		});
+	};
+	
+	_async.waterfall([checkAuth, getArticle, updateHit]);
 };
