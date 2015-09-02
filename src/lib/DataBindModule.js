0f1a6a2b85c09ed1eb8e1784b23633e446fbc39e
Alprensia <tonite32@gmail.com>
2015-09-02 21:54:33 +0900

    change data-bind module and ArticleModule

--- src/lib/DataBindModule.js
+++ src/lib/DataBindModule.js
@@ -6,16 +6,16 @@ var DataBindModule = function()
 {
 	if(DataBindModule.caller != DataBindModule.getInstance)
 		throw Error('this DataBindModule cannot be instanciated');
-	
+
 	this.modules = {};
-	
+
 	this.loadCustomHelper();
 };
 
 DataBindModule.prototype.loadCustomHelper = function()
 {
 	var path = _path.content + "/frame/" + _config.frame + "/properties/handlebars/";
-	
+
 	try
 	{
 		var result = fs.readdirSync(path);
@@ -32,8 +32,15 @@ DataBindModule.prototype.loadCustomHelper = function()
 
 DataBindModule.prototype.databind = function($, el, req, callback)
 {
-	var list = $(el).find("*[data-bind]");
-	this.compile($, list, req, callback, 0);
+	var targetList = $(el).find("*[data-bind]");
+
+	var list = [];
+	for(var i=0; i<targetList.length; i++)
+	{
+		list.push(targetList[i]);
+	}
+
+	this.compile($, list, req, callback);
 };
 
 DataBindModule.prototype.getTemplate = function($, el)
@@ -46,11 +53,11 @@ DataBindModule.prototype.getTemplate = function($, el)
 		{
 			var template = $("#" + templateId);
 			html = template.html();
-			
+
 			if(template.attr("data-precompile") == "true")
 			{
 				//프리컴파일형식으로 만들어두자?
-			}	
+			}
 			else
 			{
 				template.remove();
@@ -60,18 +67,18 @@ DataBindModule.prototype.getTemplate = function($, el)
 		{
 			if($(el).attr("data-precompile") == "true")
 			{
-				
+
 			}
-			
+
 			html = $(el).html().replace(/\\&quot;/gi, "\"");
 			html = html.replace(/&apos;/gi, "\'");
 		}
-		
+
 		$(el).removeAttr("data-template-id");
-		
+
 		if(!html)
 			html = "<p class='databind-error'>Empty template!</p>";
-		
+
 		var matchs = html.match(/{{[^}]*}}/gi);
 		if(matchs)
 		{
@@ -88,7 +95,7 @@ DataBindModule.prototype.getTemplate = function($, el)
 	{
 		_log.error(err.stack);
 	}
-	
+
 	return null;
 };
 
@@ -105,7 +112,7 @@ DataBindModule.prototype.getParameters = function($, el)
 			if(param == null)
 				param = {};
 		}
-		
+
 		$(el).removeAttr("data-param");
 	}
 	catch(err)
@@ -114,34 +121,34 @@ DataBindModule.prototype.getParameters = function($, el)
 		$(el).html("<span> ParseException : <strong>" + value + "</strong> </span><br/><br/><pre>" + err.stack + "</pre>");
 		param = null;
 	}
-	
+
 	return param;
 };
 
-DataBindModule.prototype.compile = function($, list, req, callback, index)
+DataBindModule.prototype.compile = function($, list, req, callback)
 {
 	var that = this;
-	if(index >= list.length)
+	if(list.length <= 0)
 	{
 		callback($.html());
 	}
 	else
 	{
-		var el = list[index];
-		
+		var el = list.pop();
+
 		this.databind($, el, req, function() // el의 innerHTML로 템플릿이 작성되어있는경우 data-bind가 발견될 수 있다. 그럼 끝까지 파고들어서 맨 마지막 data-bind부터 실행함.
 		{
 			//마지막 data-bind el의 innerHTML에 data-bind가 없는 경우 아래 코드가 수행됨
-			
+
 			var name = $(el).attr("data-bind");
 			$(el).removeAttr("data-bind");
-			
+
 			if(that.modules.hasOwnProperty(name)) // data-bind 모듈체크 후
 			{
 				var param = that.getParameters($, el);
 				if(param == null)
 				{
-					that.compile($, list, req, callback, index+1); // 파라미터 가져오는도중 파싱에러가 발생하면 다음 시블링의 data-bind로 넘어간다.
+					that.compile($, list, req, callback); // 파라미터 가져오는도중 파싱에러가 발생하면 다음 시블링의 data-bind로 넘어간다.
 				}
 				else
 				{
@@ -151,7 +158,7 @@ DataBindModule.prototype.compile = function($, list, req, callback, index)
 						{
 							that.databind($, el, req, function() //script로 템플릿이 작성된경우는 위에서 검출이 안된다. 따라서 data-bind 후 템플릿이 실제로 innerHTML로 만들어졌을 때 data-bind가 또 있는지 체크하고 수행함
 							{
-								that.compile($, list, req, callback, index+1); //끝나면 다음 시블링 data-bind로 넘어감.
+								that.compile($, list, req, callback); //끝나면 다음 시블링 data-bind로 넘어감.
 							});
 						});
 					}
@@ -159,11 +166,11 @@ DataBindModule.prototype.compile = function($, list, req, callback, index)
 					{
 						//모듈 호출시 오류 발생하는경우
 						_log.error(err.stack);
-						
+
 						//다음으로 넘어감
 						that.databind($, el, req, function()
 						{
-							that.compile($, list, req, callback, index+1); //끝나면 다음 시블링 data-bind로 넘어감.
+							that.compile($, list, req, callback); //끝나면 다음 시블링 data-bind로 넘어감.
 						});
 					}
 				}
@@ -172,7 +179,7 @@ DataBindModule.prototype.compile = function($, list, req, callback, index)
 			{
 				$(el).removeAttr("data-param").removeAttr("data-template-id");
 				$(el).html("<span> ModuleNotFoundException : " + name + "</span>"); // 모듈이 없는경우 알려주기 위해..
-				that.compile($, list, req, callback, index+1);
+				that.compile($, list, req, callback);
 			}
 		});
 	}
@@ -189,7 +196,7 @@ DataBindModule.getInstance = function()
 {
 	if(this.instance == null)
 		this.instance = new DataBindModule();
-	
+
 	return this.instance;
 };
 
--- src/module/ArticleModule.js
+++ src/module/ArticleModule.js
@@ -10,7 +10,7 @@ module.exports.articleList = function($, el, param, req, next)
 {
 	var that = this;
 	var boardId = param.boardId;
-	
+
 	var searchData = param.searchData;
 	var checkAuth = function(cb)
 	{
@@ -34,7 +34,7 @@ module.exports.articleList = function($, el, param, req, next)
 			}
 		});
 	};
-	
+
 	var getArticleList = function(cb)
 	{
 		ArticleDao.getArticleList(boardId, searchData, function(list)
@@ -44,7 +44,7 @@ module.exports.articleList = function($, el, param, req, next)
 			next();
 		});
 	}
-	
+
 	_async.waterfall([checkAuth, getArticleList]);
 };
 
@@ -53,7 +53,7 @@ module.exports.articleComponent = function($, el, param, req, next)
 	var that = this;
 	var boardId = param.boardId;
 	var registerDateType = param.registerDateType;
-	
+
 	var checkAuth = function(cb)
 	{
 		BoardAuthDao.getBoardAuth(boardId, function(response)
@@ -76,19 +76,19 @@ module.exports.articleComponent = function($, el, param, req, next)
 			}
 		});
 	};
-	
+
 	var pageIndex = req.query.pageIndex;
 	if(!pageIndex || isNaN(pageIndex))
 		pageIndex = 1;
-	
+
 	var searchData = param.searchData;
 	var cpp = param.cpp;
-	
+
 	if(cpp)
 		cpp = parseInt(cpp);
 	else
 		cpp = 10;
-	
+
 	if(searchData == null)
 		searchData = {};
 
@@ -101,60 +101,60 @@ module.exports.articleComponent = function($, el, param, req, next)
 				tag += " AND ";
 			tag += "TAG REGEXP '" + searchData.tag[i] + "'";
 		}
-		
+
 		searchData.tag = tag;
 	}
-	
+
 	var data = {
 		boardId : boardId,
 		searchData : searchData
 	};
-	
+
 	data.searchData.registerDateType = registerDateType;
-	
+
 	var setPagination = function(cb)
 	{
 		if(param.pagination != "false")
 		{
 			var pgc = param.pgc;
-			
+
 			if(pgc != null && pgc != "")
 				pgc = parseInt(pgc);
 			else
 				pgc = 10;
-			
+
 			if(req.session.user != null)
 			{
 				data.searchData.signinUserId = req.session.user.id;
 				data.searchData.signinUserLevel = req.session.user.level;
 			}
-			
+
 			ArticleDao.getArticleListCount(data.boardId, data.searchData, function(totalCount)
 			{
 				var maxPage = Math.ceil(totalCount / cpp);
 				maxPage = maxPage == 0 ? 1 : maxPage;
-					
-				
+
+
 				var startPage = Math.ceil(pageIndex / pgc);
 				startPage = startPage + (startPage - 1) * (pgc - 1);
-				
+
 				var pageList = [];
 				for(var i=startPage; i<startPage + pgc && i <= maxPage; i++)
 				{
 					pageList.push({pageNumber : i, href : "?piece=" + req.query.piece + "&boardId=" + data.boardId + "&pageIndex=" + i});
 				}
-				
+
 				$(el).find("*[data-parts='pagination']").each(function()
 				{
 					$(this).removeAttr("data-parts");
-					
+
 					var template = that.getTemplate($, this);
 					$(this).html(template({pageList : pageList, pageIndex : parseInt(pageIndex), maxPage : maxPage}));
 				});
-				
+
 				cb();
 			});
-			
+
 //				var totalCount = $.api.article.getArticleListCount(data);
 		}
 		else
@@ -162,12 +162,12 @@ module.exports.articleComponent = function($, el, param, req, next)
 			cb();
 		}
 	}
-	
+
 	var setArticleList = function(cb)
 	{
 		data.searchData.cpp = cpp;
 		data.searchData.pageIndex = pageIndex;
-		
+
 		if(param.searchData != null)
 		{
 			var searchData = param.searchData;
@@ -176,12 +176,12 @@ module.exports.articleComponent = function($, el, param, req, next)
 				data.searchData[key] = searchData[key];
 			}
 		}
-		
+
 		if(param.withContent == "true")
 			data.searchData.withContent = "true";
-		
+
 //			var list = $.api.article.__getArticleList(data);
-		
+
 		try
 		{
 			if(pageIndex != null && cpp != null)
@@ -195,13 +195,13 @@ module.exports.articleComponent = function($, el, param, req, next)
 			data.searchData.startIndex = null;
 			data.searchData.endIndex = null;
 		}
-		
+
 		if(req.session.user != null)
 		{
 			data.searchData.signinUserId = req.session.user.id;
 			data.searchData.signinUserLevel = req.session.user.level;
 		}
-		
+
 		ArticleDao.getArticleList(data.boardId, data.searchData, function(list)
 		{
 			var divideCount = parseInt(param.divideCount);
@@ -217,7 +217,7 @@ module.exports.articleComponent = function($, el, param, req, next)
 						template = that.getTemplate($, bodyList[index]);
 						bodyList[index].template = template;
 					}
-					
+
 					$(bodyList[index++]).append(template(list[i]));
 					if(index == divideCount)
 						index = 0;
@@ -228,23 +228,23 @@ module.exports.articleComponent = function($, el, param, req, next)
 				$(el).find("*[data-parts='body']").each(function()
 				{
 					$(this).removeAttr("data-parts");
-					
+
 					var template = that.getTemplate($, this);
 					$(this).html(template({articleList : list}));
 				});
 			}
-			
+
 			next();
 		});
 	};
-	
+
 	_async.waterfall([checkAuth, setPagination, setArticleList]);
 };
 
 module.exports.article = function($, el, param, req, next)
 {
 	var that = this;
-	
+
 	var boardId = param.boardId;
 	var seq = param.seq;
 
@@ -270,7 +270,7 @@ module.exports.article = function($, el, param, req, next)
 			}
 		});
 	};
-	
+
 	var getArticle = function(cb)
 	{
 		var searchData = {};
@@ -279,7 +279,7 @@ module.exports.article = function($, el, param, req, next)
 			searchData.signinUserId = req.session.user.id;
 			searchData.signinUserLevel = req.session.user.level;
 		}
-		
+
 		ArticleDao.getArticle(boardId, seq, searchData, function(article)
 		{
 			if(article)
@@ -291,28 +291,23 @@ module.exports.article = function($, el, param, req, next)
 						var template = that.getTemplate($, el);
 						$(el).html(template(article));
 						cb();
+						return;
 					}
 					else
 					{
+						$(el).html("<p class='databind-error'>Access denied.</p>");
 						next();
+						return;
 					}
 				}
-				else
-				{
-					var template = that.getTemplate($, el);
-					$(el).html(template(article));
-					cb();
-				}
 			}
-			else
-			{
-				var template = that.getTemplate($, el);
-				$(el).html(template(article));
-				next();
-			}
+
+			var template = that.getTemplate($, el);
+			$(el).html(template(article));
+			next();
 		});
 	};
-	
+
 	var updateHit = function(cb)
 	{
 		var userId = null;
@@ -320,7 +315,7 @@ module.exports.article = function($, el, param, req, next)
 			userId = req.session.user.id;
 		else
 			userId = (req.headers['x-forwarded-for'] || req.connection.remoteAddress) + "__" + req.headers['user-agent'];
-		
+
 		ArticleReaderDao.getArticleReader(boardId, seq, userId, function(articleReader)
 		{
 			if(articleReader == null)
@@ -329,9 +324,9 @@ module.exports.article = function($, el, param, req, next)
 				vo.boardId = boardId;
 				vo.articleSeq = seq;
 				vo.userId = userId;
-				
+
 				ArticleReaderDao.insertArticleReader(vo);
-				
+
 				ArticleDao.updateHit(vo.boardId, vo.articleSeq, function()
 				{
 					next();
@@ -343,6 +338,6 @@ module.exports.article = function($, el, param, req, next)
 			}
 		});
 	};
-	
+
 	_async.waterfall([checkAuth, getArticle, updateHit]);
 };
